a8c25945d563b62a3bfa0f72638b820b50e783e7
denis0bej <bejan.topse.denis@gmail.com>
2025-01-10 06:52:21 +0200

    Add files via upload

---
+++ UserFS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+while true; do
+    test -d ~/Desktop/root || mkdir ~/Desktop/root
+ 
+    for user in `users`; do
+        test -d ~/Desktop/root/$user || mkdir ~/Desktop/root/$user
+        ps -u "$user" > ~/Desktop/root/$user/procs
+    done
+ 
+    for user in $(ls ~/Desktop/root); do
+        if ! who | awk '{print $1}' | grep -q "^$user$"; then
+            lastlog -u "$user" | awk 'NR==2 {print $4" "$5" "$6" "$7" "$8" "$9}' > ~/Desktop/root/$user/lastlogin
+            : > ~/Desktop/root/$user/procs  # Golire fișier procs
+        fi
+    done
+ 
+    sleep 30
+done
